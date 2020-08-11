d801bff98adb5792dcccae006e543584931d8a45
Louie <ljanc001@fiu.edu>
2020-08-11 19:39:13 -0400

    Updates

---
+++ precache-manifest.921fc3fd2658b6dfc320b5021b933939.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e20b19ff7a65e2535e26526ad33f39",
+    "url": "/AI-Emojis/index.html"
+  },
+  {
+    "revision": "6df8d53de840654a2429",
+    "url": "/AI-Emojis/static/css/main.d2015af3.chunk.css"
+  },
+  {
+    "revision": "4559cf55cf4b57a9c93a",
+    "url": "/AI-Emojis/static/js/2.f748b66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AI-Emojis/static/js/2.f748b66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df8d53de840654a2429",
+    "url": "/AI-Emojis/static/js/main.cf73c954.chunk.js"
+  },
+  {
+    "revision": "e33e19b21079d02e052f",
+    "url": "/AI-Emojis/static/js/runtime-main.183272f1.js"
+  },
+  {
+    "revision": "5cf585fb2a38b17195dae89817db5b7f",
+    "url": "/AI-Emojis/static/media/hero-bg.5cf585fb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2db714ecb42dfbdb055fed9bd84ad24f.js"
+  "/AI-Emojis/precache-manifest.921fc3fd2658b6dfc320b5021b933939.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AI-Emojis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
