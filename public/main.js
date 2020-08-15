9578181ade81a3702fce36b664cdac32b1298e9e
Louie <ljanc001@fiu.edu>
2020-08-14 20:35:52 -0400

    Added Firebox browser compatibility

--- public/main.js
+++ public/main.js
@@ -5,15 +5,52 @@ const app = document.getElementById('app')
 
 //Start video
 startVideo = () => {
+	// Older browsers might not implement mediaDevices at all, so we set an empty object first
+	if (navigator.mediaDevices === undefined) {
+		navigator.mediaDevices = {}
+	}
+
+	// Some browsers partially implement mediaDevices. We can't just assign an object
+	// with getUserMedia as it would overwrite existing properties.
+	// Here, we will just add the getUserMedia property if it's missing.
+	if (navigator.mediaDevices.getUserMedia === undefined) {
+		navigator.mediaDevices.getUserMedia = function (constraints) {
+			// First get ahold of the legacy getUserMedia, if present
+			var getUserMedia =
+				navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+
+			// Some browsers just don't implement it - return a rejected promise with an error
+			// to keep a consistent interface
+			if (!getUserMedia) {
+				return Promise.reject(
+					new Error('getUserMedia is not implemented in this browser')
+				)
+			}
+
+			// Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+			return new Promise(function (resolve, reject) {
+				getUserMedia.call(navigator, constraints, resolve, reject)
+			})
+		}
+	}
+
 	navigator.mediaDevices
-		.getUserMedia({ video: {} })
-		.then(
-			(stream) => {
+		.getUserMedia({ video: true })
+		.then(function (stream) {
+			// Older browsers may not have srcObject
+			if ('srcObject' in video) {
 				video.srcObject = stream
-			},
-			(err) => console.error(err)
-		)
-		.catch((e) => console.log(e))
+			} else {
+				// Avoid using this in new browsers, as it is going away.
+				video.src = window.URL.createObjectURL(stream)
+			}
+			video.onloadedmetadata = function (e) {
+				video.play()
+			}
+		})
+		.catch(function (err) {
+			console.log(err.name + ': ' + err.message)
+		})
 }
 
 Promise.all([
@@ -72,7 +109,7 @@ video.addEventListener('play', () => {
 			})
 		} else {
 			//If not face was detected
-
+			
 			//Set default emoji
 			emoji.innerHTML = statusIcons['default'].emoji
 
